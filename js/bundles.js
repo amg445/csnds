7b0b544a9f705336eaefe04365893a9c5978a4fd
Michael Gingras <mcg79@cornell.edu>
2017-05-05 15:30:16 -0400

    changes

--- js/bundles.js
+++ js/bundles.js
@@ -1,12 +1,9 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var song_url;
 var analyzer;
-var audio;
 
 var analyze = require('web-audio-analyser');
 var scr     = require('soundcloud-resolve');
-
-$('#js-pause').on('click', function() { console.log('p'); if(Audio){audio.pause();} });
 
 require('soundcloud-badge')({
     client_id: '69c7d2345ab042057a4f19617b8028bb'
@@ -24,7 +21,7 @@ require('soundcloud-badge')({
 
 
 function init() {
-  audio  = new Audio
+  var audio  = new Audio
   audio.crossOrigin = 'Anonymous'
   audio.src = song_url
   audio.loop = true
@@ -37,12 +34,12 @@ function init() {
     console.log(waves);
     var spec = waves.map(function(m) {
       var val = m/255; return val; });
-    audio.play()
+    // audio.play()
   })
 }
 
 function updateWebGL(time) {
-  gl.clearColor(0.13, 0.13, 0.13, 1.0);
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   if(analyzer){
--- js/modules.js
+++ js/modules.js
@@ -1,12 +1,19 @@
 var song_url;
 var analyzer;
+var audio;
 
 var analyze = require('web-audio-analyser');
+var scr     = require('soundcloud-resolve');
+
+
+//TODO: Make it play again...
+// only pauses for now bc its annoying listen to 1000 times
+$('#js-pause').on('click', function() { console.log('p'); if(Audio){audio.pause();} });
 
 require('soundcloud-badge')({
     client_id: '69c7d2345ab042057a4f19617b8028bb'
   , song: 'https://soundcloud.com/itndylan/lemons-dylan'
-  , dark: true
+  , dark: false
   , getFonts: true
 }, function(err, src, data, div) {
   if (err) throw err
@@ -19,7 +26,7 @@ require('soundcloud-badge')({
 
 
 function init() {
-  var audio  = new Audio
+  audio  = new Audio
   audio.crossOrigin = 'Anonymous'
   audio.src = song_url
   audio.loop = true
@@ -37,14 +44,14 @@ function init() {
 }
 
 function updateWebGL(time) {
-  gl.clearColor(0.15, 0.2, 0.5, 1.0);
+  gl.clearColor(0.13, 0.13, 0.13, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   if(analyzer){
     var waves = analyzer.waveform();
     var spec = waves.map(function(m) {
       var val = m/255; return val; });
-    updateScene(scene, waves, spec, scaleFactor);
+    // updateScene(scene, waves, spec, scaleFactor);
   }
 
   // Update Scene based on song data
