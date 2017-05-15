f3e53bfdeb8f4f7be59941693f3dab817847ad8c
Michael Gingras <mcg79@cornell.edu>
2017-05-15 11:50:44 -0400

    cleaning

--- js/modules.js
+++ js/modules.js
@@ -3,7 +3,6 @@ var audio;
 
 var analyze = require('web-audio-analyser');
 
-
 function getSongFromURL() {
   var hash = window.location.hash;
   var song = hash.substring(1);
@@ -70,36 +69,3 @@ function init() {
     audio.play()
   })
 }
-
-// function updateWebGL(time) {
-//   gl.clearColor(0.13, 0.13, 0.13, 1.0);
-//   gl.clear(gl.COLOR_BUFFER_BIT);
-//
-//   if(analyzer){
-//     var waves = analyzer.waveform();
-//     var spec = waves.map(function(m) {
-//       var val = m/255; return val; });
-//     console.log(spec);
-//     updateScene(scene, waves, spec, scaleFactor,threshold);
-//   }
-//
-//   // Update Scene based on song data
-//   if (fft != null && sound != null && sound.isPlaying()) {
-//     // Song is playing
-//     var spectrum = fft.analyze();
-//     console.log(spectrum);
-//     var waveform = fft.waveform();
-//     updateScene(scene, spectrum, waveform, scaleFactor,threshold);
-//
-//     //var s = Math.abs(fft.waveform()[0]) * 10;
-//     //scene.particles.scale = [s, s, s];
-//     //updateToWorldMatrix(scene.particles);
-//   }
-//
-//   // Draw Scene
-//   drawScene(gl, scene);
-//
-//   // Reschedule the next frame.
-//   window.requestAnimationFrame(updateWebGL);
-// }
-// window.requestAnimationFrame(updateWebGL);
